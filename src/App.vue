d38fa81302b8c33fb55472d92aa578acee3fc525
ricardoCs7 <r.carrasco05@ufromail.cl>
2021-11-18 23:08:48 -0300

    agregado el grid y el comicCard.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
 import Comics from './components/Comics.vue'
 
 export default {
---
+++ src/components/ComicCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <div>
+      <article>
+        <h2>{{ comic.title }}</h2>
+        <img
+          :src="comic.thumbnail.path + '.jpg'"
+          :alt="comic.title"
+          height="250"
+          width="250"
+        />
+        <span>Price: {{ comic.prices[0].price }}</span>
+      </article>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  props: {
+    comic: { Object, required: true },
+  },
+};
+</script>
+<style >
+</style>
