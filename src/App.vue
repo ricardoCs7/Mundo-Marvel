dfcf0ecac9d40ab4d98be4b87c9845312551b39b
ricardoCs7 <r.carrasco05@ufromail.cl>
2021-11-19 00:02:19 -0300

    agregado el navbar con logo

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,30 @@ export default {
 </script>
 
 <template>
+<nav id="nav" class="navbar navbar-expand-sm navbar-dark bg-dark">
+  <div class="container-fluid">
+  <img src="https://logodownload.org/wp-content/uploads/2017/05/marvel-logo-4-1.png" alt="Marvel">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+      </ul>
+      <span class="navbar-text">
+        Navbar text with an inline element
+      </span>
+    </div>
+  </div>
+</nav>
   <Comics />
 </template>
 
