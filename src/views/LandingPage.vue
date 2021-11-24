00c30c4167a30f5240abe0a731e416852ce329c8
ricardoCs7 <r.carrasco05@ufromail.cl>
2021-11-23 22:06:43 -0300

    agregado Landing Page

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,12 +1,84 @@
 <template>
-    <div>
-    <h1><b>Landing Page</b></h1>
+  <section>
+    <div id="container1">
+      <img
+        id="logoLanding"
+        src="https://www.linformaldesign.com/shop/wp-content/uploads/2021/03/marvel-logo-4.png"
+        alt="Mundo Marvel"
+      />
     </div>
+    <div id="container2">
+      <div class="row">
+        <div class="col-md">
+          <img
+            src="http://www.tomacurso.com/img/portfolio/electronica.png"
+            width="140px"
+            alt=""
+          />
+          <div id="info">INFO 1</div>
+        </div>
+        <div class="col-md">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/1543/1543271.png"
+            width="140px"
+            alt=""
+          />
+          <div id="info2">INFO 2</div>
+        </div>
+        <div class="col-md">
+          <img
+            src="https://logos-world.net/wp-content/uploads/2020/11/Spider-Man-Emblem.png"
+            width="240px"
+            alt=""
+          />
+          <div id="info">INFO 3</div>
+        </div>
+        <div class="col-md">
+          <img
+            src="https://cdn-icons.flaticon.com/png/512/2234/premium/2234489.png?token=exp=1637713203~hmac=30057bb1c972e316a74163367029edfc"
+            width="140px"
+            alt=""
+          />
+          <div id="info2">INFO 4</div>
+        </div>
+      </div>
+    </div>
+    <div id="container3">
+      <div class="row">
+        <div id="info" class="col-md">
+          INFO
+          <br>
+          INFO
+          <br>
+          INFO
+        </div>
+        <div  id="info2" class="col-md">
+          INFO
+          <br>
+          INFO
+          <br>
+          INFO
+        </div>
+        <div  id="info" class="col-md">
+         INFO
+          <br>
+          INFO
+          <br>
+          INFO
+        </div>
+        <div id="info2" class="col-md">
+           INFO
+          <br>
+          INFO
+          <br>
+          INFO
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
 <style>
 </style>
