39083ea48033267011f3ef5551f2d669ba489d63
ricardoCs7 <r.carrasco05@ufromail.cl>
2021-11-30 12:22:15 -0300

    arreglada la visat en especifico en "comicInfo.vue"

--- src/views/ComicInfo.vue
+++ src/views/ComicInfo.vue
@@ -1,15 +1,9 @@
 <template>
-  <div>
-    <span style="color: red">
-      <b>{{ $route.params.id }}</b>
-    </span>
-    <span
-      >ESTE ES LA ID DE LA RUTA, PERO NO SE COMO PONERLA EN EL GET BY ID</span
-    >
-    <p>{{ this.id }}</p>
-    <p>{{ comic.id }}</p>
-    <p>{{ comic.title }}</p>
+  <div v-for="comic in comics" :comic="comic">
+    <h1>{{ comic.title }}</h1>
+    
   </div>
+
   <button @click="$router.go(-1)" type="button" class="btn btn-danger">
     <i class="bi bi-arrow-left-circle"></i> Regresar
   </button>
@@ -19,19 +13,19 @@ import marvelApi from "@/services/marvelApi.js";
 export default {
   data() {
     return {
-      comic: {},
+      comics: [],
       id: {},
     };
   },
   mounted() {
-    this.id = this.$route.params.id;
+    this.getComic(this.$route.params.id);
   },
   methods: {
     async getComic(id) {
       fetch(marvelApi.getComic(id))
         .then((res) => res.json())
         .then((json) => {
-          this.comics.push(json.data.results);
+          this.comics.push(json.data.results[0]);
           //console.log(json.data.results[0])
         });
     },
